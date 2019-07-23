e2fb3ff672f22a2cf8dc85d692cef04bcd3edec0
Jasper De gooijer <jasperdegooijer@jaspers-mbp.localdomain>
2019-07-23 18:22:34 +0200

    import the HashMap module

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,17 @@
+use std::collections::HashMap;
+
 #[near_bindgen]
 #[derive(Default, Serialize, Deserialize)]
 pub struct MyContract {
     data: HashMap<u64, u64>
 }
 
-#[near_bindgen]
-impl MyContract {
-   pub fn insert_data(&mut self, key: u64, value: u64) -> Option<u64> {
-       self.data.insert(key)
-   }
-   pub fn get_data(&self, key: u64) -> Option<u64> {
-       self.data.get(&key).cloned()
-   }
-}
+// #[near_bindgen]
+// impl MyContract {
+//    pub fn insert_data(&mut self, key: u64, value: u64) -> Option<u64> {
+//        self.data.insert(key)
+//    }
+//    pub fn get_data(&self, key: u64) -> Option<u64> {
+//        self.data.get(&key).cloned()
+//    }
+// }
