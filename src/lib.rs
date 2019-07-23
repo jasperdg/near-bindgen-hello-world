35f19f7a927ecad5d31a836bc37d76b68431ea31
Jasper De gooijer <jasperdegooijer@jaspers-mbp.localdomain>
2019-07-23 18:15:32 +0200

    initial contract setup - insolvend

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+#[near_bindgen]
+#[derive(Default, Serialize, Deserialize)]
+pub struct MyContract {
+    data: HashMap<u64, u64>
+}
+
+#[near_bindgen]
+impl MyContract {
+   pub fn insert_data(&mut self, key: u64, value: u64) -> Option<u64> {
+       self.data.insert(key)
+   }
+   pub fn get_data(&self, key: u64) -> Option<u64> {
+       self.data.get(&key).cloned()
+   }
+}
